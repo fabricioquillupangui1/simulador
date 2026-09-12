cd7cef4f721be11460723e46220fe6c76550da94
fabricioquillupangui1 <fquillupangui19@gmail.com>
2026-09-11 22:32:07 -0500

    referenciar en el html los archivos js, agregar funciones calcular y calcularDisponible respectivamente ,pruebas bien hechas,avance hasta el punto 3

--- simulador.js
+++ simulador.js
@@ -1,1 +1,12 @@
-//AQUI EL JAVASCRIPT PARA MANIPULAR EL HTML
+
+function calcular() {
+    // 1. Leer valores de entrada (ingresos y egresos como float)
+    let ingresos = parseFloat(document.getElementById("txtIngresos").value) || 0;
+    let egresos = parseFloat(document.getElementById("txtEgresos").value) || 0;
+
+    // 2. Calcular disponible y mostrar en pantalla (Paso 2 y 3)
+    let disponible = calcularDisponible(ingresos, egresos);
+    document.getElementById("spnDisponible").textContent = "USD " + disponible.toFixed(2);
+
+    
+}
